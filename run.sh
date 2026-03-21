f7f10b8c60c7ef3b25de80adfea53ead699370d0
Yin Chen <56926538+cyinen@users.noreply.github.com>
2026-03-21 12:19:35 +0800

    Change timezone for run script date logging

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "===== Start Run: $(date) ====="
+echo "===== Start Run: $(TZ='Asia/Shanghai' date) ====="
 
 python main.py > log.txt 2>&1
 
-echo "===== End Run: $(date) ====="
+echo "===== End Run: $(TZ='Asia/Shanghai' date) ====="
